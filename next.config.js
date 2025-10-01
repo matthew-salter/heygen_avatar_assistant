c15cb193633ed003aa057203688d7e2446b53b4f
matthew-salter <msalter@panelitix.ai>
2025-10-01 13:08:08 +0000

    commmit next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // don’t fail deploys on Prettier/ESLint warnings
+  },
+  webpack: (config) => {
+    // ignore problematic test/data folders from some libs
+    config.externals.push({
+      fs: "commonjs fs", // stop bundling node 'fs' module
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
