dc2fdfa0728586a4d875b0c3c139df0e83210ea0
matthew-salter <msalter@panelitix.ai>
2025-10-02 11:28:53 +0000

    commit changes

--- app/a/test/page.tsx
+++ app/a/test/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import StreamingAvatar from "@heygen/streaming-avatar/lib/StreamingAvatar";
+import { StreamingAvatarClient } from "@heygen/streaming-avatar";
 
 type AvatarConfig = {
   displayName: string;
